c56e0cabe7718f1774a39af86fd8953da19cb6f4
sudhirke <sudhir.kesharwani@gmail.com>
2024-05-09 21:56:05 +0530

    Initial version

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+
+import { HashRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+
+import {
+  FluentProvider,
+  Spinner,
+  teamsLightTheme,
+  teamsDarkTheme,
+  teamsHighContrastTheme,
+} from "@fluentui/react-components";
+import { useTeams } from "@microsoft/teamsfx-react";
+
+import SampleDashboard from "./dashboards/SampleDashboard";
+import { TeamsFxContext } from "./internal/context";
+import Privacy from "./Privacy";
+import TabConfig from "./TabConfig";
+import TermsOfUse from "./TermsOfUse";
+
+/**
+ * The main app which handles the initialization and routing
+ * of the app.
+ */
+export default function App() {
+  const { loading, themeString } = useTeams()[0];
+  return (
+    <TeamsFxContext.Provider value={{ themeString }}>
+      <FluentProvider
+        id="fluent-provider"
+        theme={
+          themeString === "dark"
+            ? teamsDarkTheme
+            : themeString === "contrast"
+            ? teamsHighContrastTheme
+            : teamsLightTheme
+        }
+      >
+        <Router>
+          {loading ? (
+            <Spinner id="spinner" />
+          ) : (
+            <Routes>
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/termsofuse" element={<TermsOfUse />} />
+              <Route path="/tab" element={<SampleDashboard />} />
+              <Route path="/config" element={<TabConfig />} />
+              <Route path="*" element={<Navigate to={"/tab"} />} />
+            </Routes>
+          )}
+        </Router>
+      </FluentProvider>
+    </TeamsFxContext.Provider>
+  );
+}
---
+++ src/internal/context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const TeamsFxContext = createContext({
+  theme: undefined,
+  themeString: "",
+});
